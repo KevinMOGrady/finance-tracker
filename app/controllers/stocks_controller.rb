5d1cafefab04a1c171f44e5d8d4e7c017bfbbdbb
KevinMOGrady <kevin.ogradyk@gmail.com>
2016-05-08 20:06:07 +0000

    Added stock search to my portfolio, currently not re-rendering partial

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,9 +2,8 @@ class StocksController < ApplicationController
   
   def search
     if params[:stock]
-      # @stock = Stock.find_by_ticker(params[:stock])
-      # @stock ||= Stock.new_from_lookup(params[:stock])
-      @stock = Stock.new_from_lookup(params[:stock])
+      @stock = Stock.find_by_ticker(params[:stock])
+      @stock ||= Stock.new_from_lookup(params[:stock])
     end
     
     if @stock
