00783ec901e2bb09952b507eb95a8d3be934348f
KevinMOGrady <kevin.ogradyk@gmail.com>
2016-05-09 18:11:16 +0000

    Finished Add stocks and Ajax for UI (corrected render after submission)

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,14 +1,22 @@
 var init_stock_lookup;
 
-
 init_stock_lookup = function() {
+  $('#stock-lookup-form').on('ajax:before', function(event, data, status) {
+    show_spinner();
+  });
+  
+  $('#stock-lookup-form').on('ajax:after', function(event, data, status) {
+    hide_spinner();
+  });
+  
   $('#stock-lookup-form').on('ajax:success', function(event, data, status) {
-    $('#stock_lookup').replaceWith(data);
+    $('#stock-lookup').replaceWith(data);
     init_stock_lookup();
   });
   
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error) {
-    $('stock-lookup-results').replaceWith(' ');
+    hide_spinner();
+    $('#stock-lookup-results').replaceWith('');
     $('#stock-lookup-errors').replaceWith('Stock was not found.');
   });
 };
@@ -16,4 +24,4 @@ init_stock_lookup = function() {
 $(document).ready(function() {
 // $(document).on('page:change', function() {
     init_stock_lookup();
-})
+});
