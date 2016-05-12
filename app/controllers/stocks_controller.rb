1d6652cb3bd4cc85559e166c32b66531f36a3667
KevinMOGrady <kevin.ogradyk@gmail.com>
2016-05-12 15:43:59 +0000

    Add user and stock association and display

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,14 +1,12 @@
 class StocksController < ApplicationController
   
   def search
-    # debugger
     if params[:stock]
       @stock = Stock.find_by_ticker(params[:stock])
       @stock ||= Stock.new_from_lookup(params[:stock])
     end
     
     if @stock
-      # debugger
       # render json: @stock
       render partial: 'lookup'
     else
